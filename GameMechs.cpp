1f8a49076b7e72d01da983b7ccf0742277b75b90
janim2 <janim2@mcmaster.ca>
2023-11-22 23:41:58 -0500

    Itteration 1B

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -2,12 +2,14 @@
 
 GameMechs::GameMechs()
 {
-
+    boardSizeX = 30;
+    boardSizeY = 15;
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
-
+    boardSizeX = boardX;
+    boardSizeY = boardY;
 }
 
 // do you need a destructor?
@@ -16,38 +18,55 @@ GameMechs::GameMechs(int boardX, int boardY)
 
 bool GameMechs::getExitFlagStatus()
 {
+    return exitFlag;
+}
 
+bool GameMechs::getLoseFlagStatus()
+{
+    return loseFlag;
 }
 
 char GameMechs::getInput()
 {
-
+    return input;
 }
 
 int GameMechs::getBoardSizeX()
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY()
 {
-
+    return boardSizeY;
 }
-
 
 void GameMechs::setExitTrue()
 {
+    exitFlag = true;
+}
 
+void GameMechs::setLoseFlag()
+{
+    loseFlag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    input = this_input;
 }
 
 void GameMechs::clearInput()
 {
-
+    input = NULL;
 }
 
+void GameMechs::getScore()
+{
+    return score;
+}
 
+void GameMechs::incrementScore()
+{
+    score++;
+}
--- GameMechs.h
+++ GameMechs.h
@@ -21,16 +21,21 @@ class GameMechs
     private:
         char input;
         bool exitFlag;
+        bool loseFlag;
         
         int boardSizeX;
         int boardSizeY;
+
+        int score;
 
     public:
         GameMechs();
         GameMechs(int boardX, int boardY);
         
         bool getExitFlagStatus();
+        bool getLoseFlagStatus();
         void setExitTrue();
+        void setLoseFlag();
 
         char getInput();
         void setInput(char this_input);
@@ -38,7 +43,9 @@ class GameMechs
 
         int getBoardSizeX();
         int getBoardSizeY();
-      
+
+        int getScore();
+        void incrementScore();
 
 };
 
